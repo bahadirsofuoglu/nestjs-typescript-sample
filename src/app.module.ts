b47dc954d9c3f8049230d8d63f050ecb52229875
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-04-24 19:23:49 +0300

    changes for nest config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,9 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [MongooseModule.forRoot(''), TodoModule],
+  imports: [ConfigModule.forRoot(),MongooseModule.forRoot(process.env.DB_URI), TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
