8197ca0b07898c9d1e675b6f81d649d84d81f35c
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-04-24 21:06:55 +0300

    some items deleted

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
 import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [ConfigModule.forRoot(),MongooseModule.forRoot(process.env.DB_URI), TodoModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
 
